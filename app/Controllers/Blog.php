2dfb526d8ae47c96596b22c914c3b40107978197
Alex Lancer <alex@alexlancer.com>
2020-05-10 02:57:59 +0300

    restapi image upload support

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -17,16 +17,27 @@ class Blog extends ResourceController
 
 		$rules = [
 			'title' => 'required|min_length[6]',
-			'description' => 'required'
+			'description' => 'required',
+			'featured_image' => 'uploaded[featured_image]|max_size[featured_image, 1024]|is_image[featured_image]'
 		];
 
 		if(!$this->validate($rules)){
 			return $this->fail($this->validator->getErrors());
 		}else{
+
+			//Get the file
+			$file = $this->request->getFile('featured_image');
+			if(! $file->isValid())
+				return $this->fail($file->getErrorString());
+
+			$file->move('./assets/uploads');
+
 			$data = [
 				'post_title' => $this->request->getVar('title'),
 				'post_description' => $this->request->getVar('description'),
+				'post_featured_image' => $file->getName()
 			];
+
 			$post_id = $this->model->insert($data);
 			$data['post_id'] = $post_id;
 			return $this->respondCreated($data);
@@ -39,23 +50,45 @@ class Blog extends ResourceController
 	}
 
 	public function update($id = null){
-		helper(['form']);
+		helper(['form', 'array']);
 
 		$rules = [
 			'title' => 'required|min_length[6]',
-			'description' => 'required'
+			'description' => 'required',
 		];
+
+
+		$fileName = dot_array_search('featured_image.name', $_FILES);
+
+		if($fileName != ''){
+			$img = ['featured_images' => 'uploaded[featured_image]|max_size[featured_image, 1024]|is_image[featured_image]'];
+			$rules = array_merge($rules, $img);
+		}
+
+
 
 		if(!$this->validate($rules)){
 			return $this->fail($this->validator->getErrors());
 		}else{
-			$input = $this->request->getRawInput();
+			//$input = $this->request->getRawInput();
+
+
 
 			$data = [
 				'post_id' => $id,
-				'post_title' => $input['title'],
-				'post_description' => $input['description'],
+				'post_title' => $this->request->getVar('title'),
+				'post_description' => $this->request->getVar('description'),
 			];
+
+			if($fileName != ''){
+
+				$file = $this->request->getFile('featured_image');
+				if(! $file->isValid())
+					return $this->fail($file->getErrorString());
+
+				$file->move('./assets/uploads');
+				$data['post_featured_image'] = $file->getName();
+			}
 
 			$this->model->save($data);
 			return $this->respond($data);
--- app/Models/BlogModel.php
+++ app/Models/BlogModel.php
@@ -5,5 +5,5 @@ use CodeIgniter\Model;
 class BlogModel extends Model{
   protected $table = 'blog';
   protected $primaryKey = 'post_id';
-  protected $allowedFields = ['post_title','post_description'];
+  protected $allowedFields = ['post_title','post_description','post_featured_image'];
 }
